c7c7a1c056257cdf61339492b9e39bb03dc8ead1
Magda <ma.wernik@gmail.com>
2023-01-27 11:47:45 +0100

    img added

--- main.php
+++ main.php
@@ -8,7 +8,7 @@ if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
     echo "Author: " . $row["author"]. "Title: " . $row["title"]. "ISBN: " . $row["isbn"]. "Description: " . $row["description"]. "Book cover: " . "<br>";
-    echo '<img src="data:image/jpeg;base64,'.base64_encode($image->load()) .'" />';
+    echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["img"]) .'" />';
   }
 } else {
   echo "0 results";
