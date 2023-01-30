f15760c81cf2b7d5a689facc5d454b338a275f0a
juliana montano <zjulim89@gmail.com>
2023-01-30 12:35:30 +0100

    se elimino archivo main.html

--- main.php
+++ main.php
@@ -1,4 +1,30 @@
-<?php
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>FemCoders Library</title>
+</head>
+<body>
+    <div class="logo">
+        <i class="fa-solid fa-book"></i>
+        <h1>FemCoders Library</h1>
+    </div>
+    <div class="mobile-container">
+        <input type="checkbox">
+        <i class="fa fa-bars"></i>
+        <i class="fa fa-times"></i>
+        <div class="myLinks">
+          <a href="./main.php">Home</a>
+          <a href="./addbook.php">Add Books</a>
+        </div>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            </a>
+        </div>
+    </div>
+    <?php
 include("connection.php");
 
 //$sql = "SELECT author, title, isbn, description, img FROM books";
@@ -19,3 +45,8 @@ if (mysqli_num_rows($result) > 0) {
 
 mysqli_close($conn);
 ?>
+
+    <script src="https://kit.fontawesome.com/27198e3231.js" crossorigin="anonymous"></script>
+</body>
+</html>
+
