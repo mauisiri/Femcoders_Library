1ae60d788e93923f98bc23a6f4b3ffc029df98e2
juliana montano <zjulim89@gmail.com>
2023-01-31 15:37:44 +0100

    tamaño de imagenes

--- main.php
+++ main.php
@@ -46,7 +46,7 @@ if (mysqli_num_rows($result) > 0) {
     $itemDetail =  "Author: ".$row["author"]."Title: ".$row["title"]."ISBN: ".$row["isbn"]."</br>";
     echo $itemDetail;
 //    echo "Author: " . $row["author"]. "Title: " . $row["title"]. "ISBN: " . $row["isbn"]. "Description: " . $row//["description"]. "Book cover: " . "<br>";
-    echo '<a href="./detailbook.php?isbn='.$row["isbn"].'"> <img src="data:image/jpeg;base64,'.base64_encode( $row["img"]) .'" /></a></br>';
+    echo '<a href="./detailbook.php?isbn='.$row["isbn"].'"> <img class="img" src="data:image/jpeg;base64,'.base64_encode( $row["img"]) .'" /></a></br>';
   }
 } else {
   echo "0 results";
