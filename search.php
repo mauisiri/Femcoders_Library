4e2b12210cf71cf5f4e48e1895e6cb870c9df0bf
juliana montano <zjulim89@gmail.com>
2023-01-31 15:24:44 +0100

    header pagina de busqueda

--- search.php
+++ search.php
@@ -1,3 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/search.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>FemCoders Library</title>
+</head>
+<body>
+    <div class="logo">
+        <i class="fa-solid fa-3x fa-book"></i>
+        <h1>FemCoders Library</h1>
+    </div>
+    <div class="mobile-container">
+        <input type="checkbox">
+        <i class="fa fa-bars"></i>
+        <i class="fa fa-times"></i>
+        <div class="myLinks">
+          <a href="./main.php">Home</a>
+          <a href="./addbook.php">Add Books</a>
+        </div>
 <?php
 
 include("connection.php");
@@ -46,3 +68,6 @@ else{ // if query length is less than minimum
 }
 
 ?>
+<script src="https://kit.fontawesome.com/27198e3231.js" crossorigin="anonymous"></script>
+</body>
+</html>
