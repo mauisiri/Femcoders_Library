6903ae0d052c4ad003a8480334495d3ac1e68201
AmelieLT <amelie_letron@yahoo.fr>
2023-02-02 11:48:12 +0100

    comment

--- edit.php
+++ edit.php
@@ -6,10 +6,10 @@ $isbn=$_POST['isbn'];
 $author=$_POST['author'];
 $title=$_POST['title'];
 $description = $_POST['description'];
-// $bookcover = $_FILES['bookcover']['tmp_name'];
-// if(!empty($bookcover)) {
-//     move_uploaded_file($bookcover);
-// }
+$bookcover = $_FILES['bookcover']['tmp_name'];
+if(!empty($bookcover)) {
+    move_uploaded_file($bookcover);
+}
 
 $sql="UPDATE books SET  title='$title', author='$author',description='$description' WHERE isbn='$isbn'";
 $query=mysqli_query($conn,$sql);
