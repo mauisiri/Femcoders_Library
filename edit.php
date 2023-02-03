034e80b2687b84916631786ccb6da0432b5bcc8a
AmelieLT <amelie_letron@yahoo.fr>
2023-02-03 09:00:21 +0100

    edit.php finished

--- edit.php
+++ edit.php
@@ -10,14 +10,10 @@ $description = $_POST['description'];
 if(!empty($_FILES['bookcover']['tmp_name'])) {
     $bookcover = addslashes(file_get_contents($_FILES['bookcover']['tmp_name']));
     $sql="UPDATE books SET title='$title', author='$author',description='$description', img='$bookcover'WHERE isbn='$isbn'";
+} else {
+    $sql="UPDATE books SET title='$title', author='$author',description='$description' WHERE isbn='$isbn'";
 }
 
-$sql="UPDATE books SET title='$title', author='$author',description='$description' WHERE isbn='$isbn'";
-
-
-// if($bookcover==0){
-//     $sql="UPDATE books SET  title='$title', author='$author',description='$description',  WHERE isbn='$isbn'";
-// } else {$sql="UPDATE books SET  title='$title', author='$author',description='$description', img='$bookcover' WHERE isbn='$isbn'";}
 
 
 
